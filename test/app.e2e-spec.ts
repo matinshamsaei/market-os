4d8152ba446e8fad866ae7820004bc9a227f9534
matin <matin3061379@gmail.com>
2026-06-19 14:46:55 +0330

    chore(project): Improve lint and format of the project

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -8,9 +8,7 @@ describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    const moduleFixture = await Test.createTestingModule({ imports: [AppModule] }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
@@ -21,9 +19,6 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
 });
