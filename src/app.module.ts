bc4db579dd93af1ed22d7176b67945f5f7154f02
matin <matin3061379@gmail.com>
2026-06-19 18:03:34 +0330

    chore(app): Add prisma and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+
+import { UsersModule } from './modules/users/users.module';
+
+import { PrismaModule } from './prisma/prisma.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
