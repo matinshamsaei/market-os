cad935d409c14151d4ba036f417031076f1089f6
matin <matin3061379@gmail.com>
2026-07-09 22:50:38 +0330

    chore(modules/catalog/constants): Create a constant for default product status

--- src/modules/catalog/constants/index.ts
+++ src/modules/catalog/constants/index.ts
@@ -1,5 +1,7 @@
 import { ProductStatus } from '@prisma/client';
 
+export const DEFAULT_PRODUCT_STATUS = ProductStatus.DRAFT;
+
 export const ALLOWED_STATUS_TRANSITIONS: Record<ProductStatus, ProductStatus[]> = {
   [ProductStatus.DRAFT]: [ProductStatus.PUBLISHED, ProductStatus.ARCHIVED],
   [ProductStatus.PUBLISHED]: [ProductStatus.ARCHIVED],
