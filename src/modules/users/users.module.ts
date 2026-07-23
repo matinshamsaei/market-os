8da8f1960c8030cb31580ce171681c324ad1afef
Matin Shamsaei <matinshamsaei.gh@gmail.com>
2026-07-23 14:45:33 +0330

    chore(modules/users): Handle users wallet creation

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,5 +1,6 @@
 import { forwardRef, Module } from '@nestjs/common';
 
+import { WalletModule } from '../wallet/wallet.module';
 import { AuthModule } from '../auth/auth.module';
 
 import { UsersController } from './users.controller';
@@ -7,7 +8,7 @@ import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), forwardRef(() => WalletModule)],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersService],
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,18 +1,29 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 
+import { PrismaService } from '@/database/prisma/prisma.service';
+
+import { WalletRepository } from '../wallet/wallet.repository';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly walletRepository: WalletRepository,
+    private readonly prisma: PrismaService,
+  ) {}
 
   findUserByEmail(email: string): Promise<User | null> {
     return this.usersRepository.findByEmail(email);
   }
 
   registerUser(data: Prisma.UserCreateInput): Promise<User> {
-    return this.usersRepository.create(data);
+    return this.prisma.$transaction(async (transaction) => {
+      const user = await this.usersRepository.create(data, transaction);
+      await this.walletRepository.createForUser(user.id, transaction);
+      return user;
+    });
   }
 
   findUserById(id: string): Promise<User | null> {
