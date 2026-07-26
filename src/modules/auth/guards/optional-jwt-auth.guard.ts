a4d5cbb5a8031b10799020b5ff37f28d95cc77f3
matin <matin3061379@gmail.com>
2026-07-26 14:31:29 +0330

    chore(modules/auth/guards): Add an optional jwt checker for anonymous available requests

---
+++ src/modules/auth/guards/optional-jwt-auth.guard.ts
@@ -0,0 +1,27 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<{ headers: { authorization?: string } }>();
+    if (!request.headers.authorization) {
+      return true;
+    }
+
+    try {
+      const result = await super.canActivate(context);
+      return result as boolean;
+    } catch {
+      return true;
+    }
+  }
+
+  handleRequest<TUser>(err: Error | null, user: TUser): TUser | null {
+    if (err || !user) {
+      return null;
+    }
+
+    return user;
+  }
+}
