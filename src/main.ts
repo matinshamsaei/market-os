9dc2846a1898f3a23b13c71c87f12b1757119136
Matin Shamsaei <matinshamsaei.gh@gmail.com>
2026-09-06 21:55:12 +0330

    chore(src/main): Improve nest-create and add enable-cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
